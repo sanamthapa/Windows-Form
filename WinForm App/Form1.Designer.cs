fa71dd94c9fd7281681ce5b282718dac7525172f
Sanam Thapa <40533261+sanamthapa@users.noreply.github.com>
2022-09-15 12:55:25 +0545

    Windows Form App

---
+++ WinForm App/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WinForm_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDesigination = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rbtMale = new System.Windows.Forms.RadioButton();
+            this.rbtFemale = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chkGood = new System.Windows.Forms.CheckBox();
+            this.chkVerygood = new System.Windows.Forms.CheckBox();
+            this.dtDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Desigination:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salary:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(195, 57);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(195, 27);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtDesigination
+            // 
+            this.txtDesigination.Location = new System.Drawing.Point(195, 101);
+            this.txtDesigination.Name = "txtDesigination";
+            this.txtDesigination.Size = new System.Drawing.Size(195, 27);
+            this.txtDesigination.TabIndex = 4;
+            this.txtDesigination.TextChanged += new System.EventHandler(this.txtDesigination_TextChanged);
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(195, 146);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(195, 27);
+            this.txtSalary.TabIndex = 5;
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSave.Location = new System.Drawing.Point(239, 347);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 29);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Location = new System.Drawing.Point(75, 276);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(0, 20);
+            this.lblDisplay.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 195);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Gender:";
+            // 
+            // rbtMale
+            // 
+            this.rbtMale.AutoSize = true;
+            this.rbtMale.Location = new System.Drawing.Point(195, 195);
+            this.rbtMale.Name = "rbtMale";
+            this.rbtMale.Size = new System.Drawing.Size(63, 24);
+            this.rbtMale.TabIndex = 9;
+            this.rbtMale.TabStop = true;
+            this.rbtMale.Text = "Male";
+            this.rbtMale.UseVisualStyleBackColor = true;
+            // 
+            // rbtFemale
+            // 
+            this.rbtFemale.AutoSize = true;
+            this.rbtFemale.Location = new System.Drawing.Point(312, 195);
+            this.rbtFemale.Name = "rbtFemale";
+            this.rbtFemale.Size = new System.Drawing.Size(78, 24);
+            this.rbtFemale.TabIndex = 10;
+            this.rbtFemale.TabStop = true;
+            this.rbtFemale.Text = "Female";
+            this.rbtFemale.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(75, 251);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Review:";
+            // 
+            // chkGood
+            // 
+            this.chkGood.AutoSize = true;
+            this.chkGood.Location = new System.Drawing.Point(195, 250);
+            this.chkGood.Name = "chkGood";
+            this.chkGood.Size = new System.Drawing.Size(68, 24);
+            this.chkGood.TabIndex = 12;
+            this.chkGood.Text = "Good";
+            this.chkGood.UseVisualStyleBackColor = true;
+            // 
+            // chkVerygood
+            // 
+            this.chkVerygood.AutoSize = true;
+            this.chkVerygood.Location = new System.Drawing.Point(312, 250);
+            this.chkVerygood.Name = "chkVerygood";
+            this.chkVerygood.Size = new System.Drawing.Size(100, 24);
+            this.chkVerygood.TabIndex = 13;
+            this.chkVerygood.Text = "Very Good";
+            this.chkVerygood.UseVisualStyleBackColor = true;
+            // 
+            // dtDataGridView
+            // 
+            this.dtDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtDataGridView.Location = new System.Drawing.Point(25, 437);
+            this.dtDataGridView.Name = "dtDataGridView";
+            this.dtDataGridView.RowHeadersWidth = 51;
+            this.dtDataGridView.RowTemplate.Height = 29;
+            this.dtDataGridView.Size = new System.Drawing.Size(557, 230);
+            this.dtDataGridView.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(618, 696);
+            this.Controls.Add(this.dtDataGridView);
+            this.Controls.Add(this.chkVerygood);
+            this.Controls.Add(this.chkGood);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.rbtFemale);
+            this.Controls.Add(this.rbtMale);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtDesigination);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtName;
+        private TextBox txtDesigination;
+        private TextBox txtSalary;
+        private Button btnSave;
+        private Label lblDisplay;
+        private Label label4;
+        private RadioButton rbtMale;
+        private RadioButton rbtFemale;
+        private Label label5;
+        private CheckBox chkGood;
+        private CheckBox chkVerygood;
+        private DataGridView dtDataGridView;
+    }
+}
